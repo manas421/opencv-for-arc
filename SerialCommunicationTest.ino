3d34a08a1ea2816661aa0fe8231ec10b4373cd30
manas421 <31443053+manas421@users.noreply.github.com>
2017-09-29 11:50:27 +0530

    Add files via upload
    Serial Communication code for Arduino

---
+++ SerialCommunicationTest.ino
@@ -0,0 +1,18 @@
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(57600);
+  Serial3.begin(57600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(Serial3.available()) {
+    byte b = Serial3.read();
+    Serial.print((char)b);
+  }
+  while(Serial.available()) {
+    byte b = Serial.read();
+    Serial3.write(b);
+  }
+}
